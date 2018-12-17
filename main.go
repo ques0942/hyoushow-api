87fecefbe86232e9d20bc925268a478f18f5f152
ques0942 <nari0942@gmail.com>
2018-12-18 01:55:00 +0900

    API化

--- main.go
+++ main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 
-	"github.com/ques0942/sampleapp/model"
+	"github.com/ques0942/hyoushow-api/model"
+	"github.com/throttled/throttled"
+	"github.com/throttled/throttled/store/memstore"
 )
 
 func hyoushow(receiver string, sender string, dateStr string, body string) (string, error) {
@@ -28,10 +32,65 @@ func hyoushow(receiver string, sender string, dateStr string, body string) (stri
 	return pageModel.Generate()
 }
 
-func main() {
-	resultURL, err := hyoushow("testさん", "from test", "12/01", "試しに\nあなたを\n表彰します")
-	if err != nil {
-		fmt.Println(err)
+func getParam(r *http.Request, key string) (string, error) {
+	if param, ok := r.URL.Query()[key]; ok && len(param) > 0 {
+		return param[0], nil
+	} else {
+		return "", fmt.Errorf("%s not found", key)
 	}
-	fmt.Println(resultURL)
+}
+
+func main() {
+
+	store, err := memstore.New(65536)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	quota := throttled.RateQuota{throttled.PerMin(20), 5}
+	rateLimiter, err := throttled.NewGCRARateLimiter(store, quota)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	httpRateLimiter := throttled.HTTPRateLimiter{
+		RateLimiter: rateLimiter,
+		VaryBy:      &throttled.VaryBy{Path: true},
+	}
+
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		receiver, err := getParam(r, "receiver")
+		if err != nil {
+			log.Println(err)
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("receiver not found"))
+			return
+		}
+		sender, err := getParam(r, "sender")
+		if err != nil {
+			log.Println(err)
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("sender not found"))
+			return
+		}
+		dateStr, err := getParam(r, "dateStr")
+		body, err := getParam(r, "body")
+		if err != nil || body == "" {
+			log.Println("body not found")
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("body not found"))
+			return
+		}
+		resultURL, err := hyoushow(receiver, sender, dateStr, body)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		fmt.Println(resultURL)
+		http.Redirect(w, r, resultURL, http.StatusMovedPermanently)
+	}
+	limitedFunc := httpRateLimiter.RateLimit(http.HandlerFunc(handler))
+	http.Handle("/", limitedFunc)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
